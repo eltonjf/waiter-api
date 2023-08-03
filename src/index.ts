1f721cd57b78d6f3c97da8ab64fd29bd399a8bcd
Elton Mário <eltonjf@gmail.com>
2023-08-03 00:37:42 -0300

    Conection with mongodb made

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose'
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        console.log('Conectado ao banco de dados');
+        const app = express();
+        const port = 3001;
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    })
+    .catch(() => console.log('Erro ao conectar ao mongodb'));
 
-const port = 3001;
-app.listen(port, () => {
-    console.log('Server is running on http://localhost:${port}');
-});
+
